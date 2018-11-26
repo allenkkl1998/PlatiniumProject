0ce60052c389570800961997907dc1455230dd62
allenkkl <44184169+allenkkl1998@users.noreply.github.com>
2018-11-26 10:10:04 +0800

    Add files via upload

---
+++ FioreFlowershopSystem/test/View/MonthlyPromotionItemTest.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AL
+ */
+public class MonthlyPromotionItemTest {
+    
+    public MonthlyPromotionItemTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addItemToJCBPN method, of class MonthlyPromotionItem.
+     */
+    @Test
+    public void testAddItemToJCBPN() {
+        System.out.println("addItemToJCBPN");
+      //  MonthlyPromotionItem instance = new MonthlyPromotionItem();
+        //instance.addItemToJCBPN();
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addItemToJCBNPI method, of class MonthlyPromotionItem.
+     */
+    @Test
+    public void testAddItemToJCBNPI() {
+        //System.out.println("addItemToJCBNPI");
+        //MonthlyPromotionItem instance = new MonthlyPromotionItem();
+        //instance.addItemToJCBNPI();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of createPromotionProductID method, of class MonthlyPromotionItem.
+     */
+    @Test
+    public void testCreatePromotionProductID() {
+        System.out.println("createPromotionProductID");
+//        MonthlyPromotionItem instance = new MonthlyPromotionItem();
+   //     String expResult = "";
+     //   String result = instance.createPromotionProductID();
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class MonthlyPromotionItem.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        MonthlyPromotionItem.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
