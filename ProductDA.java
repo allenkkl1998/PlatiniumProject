58239496ceef1dd51fbea10ebcc967669dbe49a2
allenkkl <44184169+allenkkl1998@users.noreply.github.com>
2018-12-11 22:09:00 +0800

    Add files via upload

---
+++ ProductDA.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Da;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import Domain.Product;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author AL
+ */
+public class ProductDA {
+    private String host = "jdbc:derby://localhost:1527/FlowerDB";
+    private String user = "nbuser";
+    private String password = "nbuser";
+    private String tableName = "Product";
+    private Connection conn;
+    private PreparedStatement stmt;
+    
+    public ProductDA(){
+    createConnection();
+    }
+    
+    public void addRecord(Product p){
+           
+            try{
+                    String insertStr="INSERT INTO "+tableName+" VALUES(?, ?, ?, ?, ? )";
+                    stmt=conn.prepareStatement(insertStr);
+                    stmt.setString(1, p.getProductname());
+                    stmt.setString(2, p.getProductdesciption());
+                    stmt.setInt(3, p.getQuantity());
+                    stmt.setString(4,p.getProducttype());
+                    stmt.setDouble(5, p.getProductprice());
+                    stmt.executeUpdate();
+                    
+                }catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, ex.getMessage(),"ERROR",JOptionPane.ERROR_MESSAGE);
+            }
+    }
+    
+    public List<Product> getAllRecord() {
+            String queryStr = "SELECT * FROM " + tableName;
+            List<Product> pList=new ArrayList<Product>();
+            Product pRecord = null;
+        try {
+            stmt = conn.prepareStatement(queryStr);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()) {                
+                pRecord = new Product(rs.getString("ProductNAME"), rs.getString("PRODUCTDESCRIPTION"),rs.getInt("QUANTITY"), rs.getString("PRODUCTTYPE"),rs.getDouble("PRODUCTPRICE") );
+                pList.add(pRecord);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+        return pList;
+    }
+    
+              public Product getAllSelectedRecordViaProductName(String productName) {
+            String queryStr = "SELECT * FROM " + tableName+" WHERE PRODUCTNAME = ? ";
+            Product pRecord = null;
+        try {
+            stmt = conn.prepareStatement(queryStr);
+            stmt.setString(1, productName);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()) {                
+                pRecord = new Product(rs.getString("ProductNAME"), rs.getString("PRODUCTDESCRIPTION"),rs.getInt("QUANTITY"), rs.getString("PRODUCTTYPE"),rs.getDouble("PRODUCTPRICE") );
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "getAllSeletedRecord ERROR",JOptionPane.ERROR_MESSAGE);
+        }
+        return pRecord;
+    }
+              
+             public void updateRecord(Product updateProduct){
+            try{                  
+                    String updateStr="UPDATE "+ tableName +" SET PRODUCTDESCRIPTION = ?, QUANTITY = ?, PRODUCTPRICE = ?, PRODUCTTYPE = ? WHERE PRODUCTNAME = ? ";
+                    stmt=conn.prepareStatement(updateStr);
+                    stmt.setString(1, updateProduct.getProductdesciption());
+                    stmt.setInt(2, updateProduct.getQuantity());
+                    stmt.setDouble(3, updateProduct.getProductprice());
+                    stmt.setString(4, updateProduct.getProducttype());
+                    stmt.setString(5, updateProduct.getProductname());
+                    stmt.executeUpdate();
+
+            }catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, ex.getMessage(),"ERROR",JOptionPane.ERROR_MESSAGE);
+            }
+        }
+             
+                   public void deleteRecord(String productName){
+            try{                  
+                    String deleteStrForPP="DELETE FROM PROMOTIONPRODUCT WHERE PRODUCTNAME = ? ";
+                    stmt=conn.prepareStatement(deleteStrForPP);
+                    stmt.setString(1, productName);
+                    stmt.executeUpdate();
+                
+                
+                    String deleteStr="DELETE FROM "+ tableName +"  WHERE PRODUCTNAME = ? ";
+                    stmt=conn.prepareStatement(deleteStr);
+                    stmt.setString(1, productName);
+                    stmt.executeUpdate();
+
+            }catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, ex.getMessage(),"ERROR",JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    
+    private void createConnection() {
+        try {
+            conn = DriverManager.getConnection(host, user, password);
+            System.out.println("***TRACE: Connection established.");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
---
+++ Promotion.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ASUS
+ */
+@Entity
+@Table(name = "PROMOTION")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Promotion.findAll", query = "SELECT p FROM Promotion p")
+    , @NamedQuery(name = "Promotion.findByPromotionname", query = "SELECT p FROM Promotion p WHERE p.promotionname = :promotionname")
+    , @NamedQuery(name = "Promotion.findByPromotionmonth", query = "SELECT p FROM Promotion p WHERE p.promotionmonth = :promotionmonth")
+    , @NamedQuery(name = "Promotion.findByPromotiondetail", query = "SELECT p FROM Promotion p WHERE p.promotiondetail = :promotiondetail")
+    , @NamedQuery(name = "Promotion.findByPromotiontc", query = "SELECT p FROM Promotion p WHERE p.promotiontc = :promotiontc")
+    , @NamedQuery(name = "Promotion.findByPyear", query = "SELECT p FROM Promotion p WHERE p.pyear = :pyear")})
+public class Promotion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "PROMOTIONNAME")
+    private String promotionname;
+    @Basic(optional = false)
+    @Column(name = "PROMOTIONMONTH")
+    private String promotionmonth;
+    @Basic(optional = false)
+    @Column(name = "PROMOTIONDETAIL")
+    private String promotiondetail;
+    @Basic(optional = false)
+    @Column(name = "PROMOTIONTC")
+    private String promotiontc;
+    @Column(name = "PYEAR")
+    private Integer pyear;
+
+    public Promotion() {
+    }
+
+    public Promotion(String promotionname) {
+        this.promotionname = promotionname;
+    }
+
+    public Promotion(String promotionname, String promotionmonth, String promotiondetail, String promotiontc,int pyear) {
+        this.promotionname = promotionname;
+        this.promotionmonth = promotionmonth;
+        this.promotiondetail = promotiondetail;
+        this.promotiontc = promotiontc;
+        this.pyear=pyear;
+    }
+
+    public String getPromotionname() {
+        return promotionname;
+    }
+
+    public void setPromotionname(String promotionname) {
+        this.promotionname = promotionname;
+    }
+
+    public String getPromotionmonth() {
+        return promotionmonth;
+    }
+
+    public void setPromotionmonth(String promotionmonth) {
+        this.promotionmonth = promotionmonth;
+    }
+
+    public String getPromotiondetail() {
+        return promotiondetail;
+    }
+
+    public void setPromotiondetail(String promotiondetail) {
+        this.promotiondetail = promotiondetail;
+    }
+
+    public String getPromotiontc() {
+        return promotiontc;
+    }
+
+    public void setPromotiontc(String promotiontc) {
+        this.promotiontc = promotiontc;
+    }
+
+    public Integer getPyear() {
+        return pyear;
+    }
+
+    public void setPyear(Integer pyear) {
+        this.pyear = pyear;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (promotionname != null ? promotionname.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Promotion)) {
+            return false;
+        }
+        Promotion other = (Promotion) object;
+        if ((this.promotionname == null && other.promotionname != null) || (this.promotionname != null && !this.promotionname.equals(other.promotionname))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Domain.Promotion[ promotionname=" + promotionname + " ]";
+    }
+    
+}
